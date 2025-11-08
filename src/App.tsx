2b2d993c8e03dac1d2cf5b5f215507573e9f364d
Geet-7 <geetlunkad@gmail.com>
2025-11-08 11:44:07 +0530

    scroll top functionality update

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import About from "./pages/About";
 import Achievements from "./pages/Achievements";
 import Gallery from "./pages/Gallery";
 import NotFound from "./pages/NotFound";
+import ScrollToTop from "./components/ScrollToTop";
 
 const queryClient = new QueryClient();
 
@@ -18,6 +19,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
